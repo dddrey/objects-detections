6f292b79f6e1e4efac75ce89402c7b79d568001f
Andrushkins <drachyov.andrew@gmail.com>
2017-05-13 15:53:49 +0300

    face detection

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const cv = require('opencv');
 
-try {
-  var camera = new cv.VideoCapture(0);
-  var window = new cv.NamedWindow('Video', 0);
-  setInterval( () => {
-    camera.read( (err, img) => {
+const fps = 1000/10;
+const rectColor = [0, 255, 0];
+const rectThickness = 2;
+
+try{
+  let cap = new cv.VideoCapture(0);
+  let window = new cv.NamedWindow('Video', 0);
+
+  let streaming = (err, img) =>{
+    if (err) throw err;
+    img.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, (err, faces) => {
       if (err) throw err;
-      // console.log(img.size());
-      if (img.size()[0] > 0 && img.size()[1] > 0){
-        window.show(img);
+
+      for (let i = 0; i < faces.length; i++) {
+        face = faces[i];
+        img.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
       }
-      window.blockingWaitKey(0, 50);
+      window.show(img);
     });
-  }, 20);
+    window.blockingWaitKey(0, 50);
+  }
+
+  setInterval( () =>{
+    cap.read(streaming);
+  }, fps);
 } catch (e){
   console.log("Couldn't start camera:", e);
 };
